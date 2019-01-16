7c6aa271f8b78643f66634632f354c2216808fd2
zhangzhentao <28299209@qq.com>
2019-01-16 08:44:14 +0800

    add init_db command

---
+++ models.py
@@ -0,0 +1,8 @@
+
+from db import db
+
+class Article(db.Model):
+    __tablename__='article'
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(100),nullable=False)
+    content = db.Column(db.Text,nullable=False)
